139bb8ef40ce57d9421eb5541b04ef5f9dd9dd5f
Ruben Suarez <daricoder@gmail.com>
2021-09-09 15:38:02 -0500

    aniadiendo explicacion

--- src/components/BotonEstilizado.js
+++ src/components/BotonEstilizado.js
@@ -1,6 +1,17 @@
 import {useContext} from "react";
 import {MiContexto} from '../context/GlobalContext';
 
+/* 
+Poniendo a prueba el contexto
+como ves este es un componente que llama al contexto a traves de useContext
+y de ahi saca todos los valores del contexto y los utiliza.
+
+Este componente es utilizado en la jerarquia de componentes como el mas profundo
+ya que el componente App utiliza el componente MiBoton y este a su vez utiliza este Componente y dentro
+de este componente queremos acceder al contexto y cambiar su estado.
+
+
+*/
 
 export const BotonEstilizado = () => {
     const {contador,incrementarContador} = useContext(MiContexto)
